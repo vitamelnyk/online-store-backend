29c4af691f191585455aad21a052c5c33e673f36
vitamelnyk <vitamelnik6@gmail.com>
2026-06-16 11:22:08 +0300

    Made registration, login and jwt generation functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,54 @@
 import ApiError from "../error/ApiError.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User, Basket } from '../models/models.js';
 
-export async function registration(req, res) {
-
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        { id, email, role },
+        process.env.SECRET_KEY,
+        { expiresIn: '24h' }
+    );
 }
 
-export async function login(req, res) {
+export async function registration(req, res, next) {
+    const { email, password, role } = req.body;
 
+    if (!email || !password) {
+        return next(ApiError.badRequest('Некорректний email или password'));
+    }
+
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+        return next(ApiError.badRequest('Пользователь с таким email уже существует'));
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+}
+
+export async function login(req, res, next) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return next(ApiError.badRequest('Некорректний email или password'));
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+        return next(ApiError.badRequest('Пользователь с таким email не найден'));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+        return next(ApiError.badRequest('Указан неверный пароль'));
+    }
+
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({token});
 }
 
 export async function check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-        return next(ApiError.badRequest("Не задан ID"))
-    }
-
-    res.json(id);
 }
